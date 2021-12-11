4ccd1856f5cb251bc9aa13c7a500d4604563a173
caseymcspadden <casey@caseymcspadden.com>
2021-12-11 09:15:00 -0600

    After PM2 setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,8 +7,8 @@ module.exports = {
     production: {
       user: 'ubuntu',
       host: 'ec2-3-145-187-124.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/nodejs-server.pem',
-      ref: 'origin/main',
+      key:  '~/.ssh/nodejs-server.pem',
+      ref:  'origin/main',
       repo: 'git@github.com:caseymcspadden/nodejs-app.git',
       path: '/home/ubuntu/nodejs-app',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
